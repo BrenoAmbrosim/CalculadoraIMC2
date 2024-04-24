37c5eed9a05758763d6e4a39c576bc6ec94d7444
BRENO AMBROSIM LOUZADA <brenoambrosim8@gmail.com>
2024-04-24 17:56:59 -0300

    Melhorando experiencia do usuario

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
                 intent.putExtra(KEY_RESULT_IMC,resultado)
                 startActivity(intent)
 
-                println("Breno Ação do botão"+ resultado)
+
             }
 
 
